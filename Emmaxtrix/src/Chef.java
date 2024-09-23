9082f7e876baaf51ba940a13abaec401b00c202b
emmarcrg <160240686+emmarcrg@users.noreply.github.com>
2024-09-23 14:42:27 +0200

    Conservateur et chef

---
+++ Emmaxtrix/src/Chef.java
@@ -0,0 +1,18 @@
+public class Chef extends Habitant {
+    private Convocation convoc;
+
+    public Chef (NNIG id, String nom, String prenom, Adresse adresse,int poids){
+        super();
+        this.identifiant = id;
+        this.nom=nom;
+        this.prenom=prenom;
+        this.adresse=adresse;
+        this.poids=poids;
+    }
+
+    @Override
+    public boolean choisirCombattre() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'choisirCombattre'");
+    }
+}
---
+++ Emmaxtrix/src/Conservateur.java
@@ -0,0 +1,25 @@
+public class Conservateur extends Habitant {
+
+    public Conservateur (NNIG id, String nom, String prenom, Adresse adresse,int poids){
+        super();
+        this.identifiant = id;
+        this.nom=nom;
+        this.prenom=prenom;
+        this.adresse=adresse;
+        this.poids=poids;
+    }
+
+    @Override
+    public boolean choisirCombattre() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'choisirCombattre'");
+    }
+        
+    public void enregistrerCasque(Habitant hab, Casque c){
+        // Méthode permettant de rajouter un casque à un habitant 
+    }
+
+    public void demanderInfos(){
+        //permet de donner la liste des casque récupérer par un habitant 
+    }
+}
