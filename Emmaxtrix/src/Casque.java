1f901a35b833ccf25477e9d1f7441c14747fef7f
Axelle <160240449+aquellery@users.noreply.github.com>
2024-09-23 14:45:20 +0200

    Casque + Combat

---
+++ Emmaxtrix/src/Casque.java
@@ -0,0 +1,18 @@
+public class Casque {
+    private String forme;
+    private String ennemi;
+    private String etat;
+    private boolean musee;
+    private Habitant hab;
+    private Combat combat;
+
+    public Casque(String f, String en, String et, boolean m, Habitant h, Combat c){
+        this.forme = f;
+        this.ennemi = en;
+        this.etat = et;
+        this.musee=m;
+        this.hab=h;
+        this.combat=c;
+    }
+
+}
