d34ee88272acbd01608f46e21f0104fb3efa921d
emmarcrg <160240686+emmarcrg@users.noreply.github.com>
2024-09-23 15:04:57 +0200

    AncienCombattant

---
+++ Emmaxtrix/src/AncienCombattant.java
@@ -0,0 +1,18 @@
+public class AncienCombattant extends Habitant{
+
+    public AncienCombattant (NNIG id, String nom, String prenom, Adresse adresse,int poids){
+        if(this.combattant){
+            this.identifiant = id;
+        this.nom=nom;
+        this.prenom=prenom;
+        this.ad=adresse;
+        this.poids=poids;
+        }
+    }
+    @Override
+    public boolean choisirCombattre() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'choisirCombattre'");
+    }
+    
+}
