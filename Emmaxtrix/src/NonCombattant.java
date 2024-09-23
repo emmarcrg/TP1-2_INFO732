6eaf4cf56fab14066cb069de24db253ef6b55207
emmarcrg <160240686+emmarcrg@users.noreply.github.com>
2024-09-23 14:57:14 +0200

    NonCombattant

---
+++ Emmaxtrix/src/NonCombattant.java
@@ -0,0 +1,10 @@
+public class NonCombattant extends Habitant{
+    public NonCombattant (NNIG id, String nom, String prenom, Adresse adresse,int poids){
+        super();
+        this.identifiant = id;
+        this.nom=nom;
+        this.prenom=prenom;
+        this.ad=adresse;
+        this.poids=poids;
+    }
+}
